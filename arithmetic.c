9edc7debf20e0f5f4ba377d76a19aed9c42ced4b
Nayana.Lalitha <106461721+NayanaLalitha67@users.noreply.github.com>
2022-05-29 11:44:06 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=10;
+	int b=3;
+	printf("add=%d",a+b);
+	printf("\n subtract=%d",a-b);
+	printf("\n multiplication=%d",a*b);
+	printf("\n division=%d",a/b);
+	printf("\n modulo=%d",a%b);
+	return 0;
+	
+}
